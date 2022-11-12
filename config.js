28e2a1bc00fd2e8bf8271e68b0e97784d1749f4b
silabantu <112931609+silabantu@users.noreply.github.com>
2022-11-12 14:00:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-global.you = ['6285850539404'] 
+global.you = ['6289509158681'] 
 //Developer Bot
-global.gc = 'https://chat.whatsapp.com/I5Kz3Abawc5DqyBSDoYvKB'
-global.rowner = ['6285850539404'] // Real Owner Bot
-global.owner = ['6285850539404'] // Owner Bot
-global.police = ['6285850539404'] // Police Bot
-global.mods = ['6285850539404']  // Want some help?
-global.prems = ['6285850539404'] // Premium user has unlimited limit
+global.gc = 'https://chat.whatsapp.com/KnbKroBBalAFdGqb1zB9mt'
+global.rowner = ['6289509158681'] // Real Owner Bot
+global.owner = ['6289509158681'] // Owner Bot
+global.police = ['994403231195'] // Police Bot
+global.mods = ['6289509158681']  // Want some help?
+global.prems = ['6289509158681'] // Premium user has unlimited limit
 global.fotonya1 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini kalo mau di ganti silahkan ganti aja
 global.fotonya2 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //gw lupa nih foto apa aja:v
 global.fotonya3 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
@@ -13,9 +13,9 @@ global.fotonya4 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga
 global.fotonya5 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
 global.fotonya6 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini ganti pakai foto sosialmedia mu
 global.fotonya7 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
-global.instagram = 'https://instagram.com/Jarotr_'
+global.instagram = 'https://instagram.com/jonkristiansah.silaban'
 
-global.helper = ['6285850539404']
+global.helper = ['6289509158681']
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
@@ -84,9 +84,9 @@ global.fsizedoc = '564588643' // default 10TB
 global.fpagedoc = '444'
 
 // Sticker WM
-global.packname = 'Jarot offc'
-global.author = 'Jarot offc'
-global.wm = '© Jarot offc'
+global.packname = 'Jonkris offc'
+global.author = 'Jonkris offc'
+global.wm = '© Jonkris offc'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
